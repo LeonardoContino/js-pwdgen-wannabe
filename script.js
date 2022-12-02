8e021206dccabf52af58940673c3464bc85d1de0
LeonardoContino <leoconti2000@hotmail.it>
2022-12-02 15:31:07 +0100

    aggiunto element

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("ok js");
+// console.log("ok js");
+
+/*
+Traccia:
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+/*
+    1. prendo elemento in dom   
+    2. chiedo all'utente il suo nome
+    3. chiedo il suo cognome
+    4. chiedo il suo colore preferito
+    5. inserisco in html o dom i dati dell'utente
+ */
+
+const targetElement = document.getElementById("password");
+// console.log(targetElement);
