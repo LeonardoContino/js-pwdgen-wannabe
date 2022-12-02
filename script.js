54fde1c6e7eaf621d3314d1b8576e3ec4df3c5bf
LeonardoContino <leoconti2000@hotmail.it>
2022-12-02 15:47:09 +0100

    aggiunto commenti

--- script.js
+++ script.js
@@ -14,14 +14,22 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21
     3. chiedo il suo cognome
     4. chiedo il suo colore preferito
     5. inserire numero 21 su dom
-    6. inserisco in html o dom i dati dell'utente
+    6. inserisco in html o dom gli elementi dell'utente
  */
-
+//prendo elemento in dom
 const targetElement = document.getElementById("password");
 // console.log(targetElement);
+
+// chiedo all'utente il suo nome
 const UserName = prompt("quale è il tuo nome?", "Leonardo");
 // console.log(UserName);
+
+// chiedo il suo cognome
 const LastName = prompt("quale è il tuo cognome?", "Contino");
 // console.log(LastName);
+
+// chiedo il suo colore preferito
 const PrefereColor = prompt("quale è il tuo colore preferito?", "blu");
 console.log(PrefereColor);
+
+// inserire elementi in html
